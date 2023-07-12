a07bd0d90530f7fd430c36abb2a05890601bef92
RM <rm@Mac-Pro-de-RM.local>
2023-07-11 20:21:16 -0500

    initial

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class RegisterPageComponent {
+
+}
