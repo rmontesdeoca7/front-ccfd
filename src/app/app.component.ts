86e9c462c2634377912af3d26be4e141202a0ae0
RM <rm@Mac-Pro-de-RM.local>
2023-07-20 22:27:02 -0500

    header ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,29 @@ import { AuthStatus } from './auth/interfaces';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private authService = inject( AuthService );
-  private router = inject( Router );
+  // private authService = inject( AuthService );
+  // private router = inject( Router );
 
-  public finishedAuthCheck = computed<boolean>(() => {
-    if ( this.authService.authStatus() === AuthStatus.checking ) {
-      return false;
-    }
-    return true;
-  });
+  // public finishedAuthCheck = computed<boolean>(() => {
+  //   if ( this.authService.authStatus() === AuthStatus.checking ) {
+  //     return false;
+  //   }
+  //   return true;
+  // });
 
-  public authStatusChangedEffect = effect( () => {
-    switch( this.authService.authStatus() ) {
-      case AuthStatus.checking:
-        return;
+  // public authStatusChangedEffect = effect( () => {
+  //   switch( this.authService.authStatus() ) {
+  //     case AuthStatus.checking:
+  //       return;
 
-      case AuthStatus.authenticated:
-        this.router.navigateByUrl('/dashboard');
-        return;
+  //     case AuthStatus.authenticated:
+  //       this.router.navigateByUrl('/dashboard');
+  //       return;
         
-      case AuthStatus.notAuthenticated:
-        this.router.navigateByUrl('/auth/login');
-        return;
+  //     case AuthStatus.notAuthenticated:
+  //       this.router.navigateByUrl('/auth/login');
+  //       return;
 
-    }
-  })
+  //   }
+  // })
 }
